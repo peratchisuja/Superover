e619646a9f6b43f1c65867a63697368d3969a3b9
unknown <peratchisuja@gmail.com>
2020-11-20 19:38:45 +0530

    day 4 js displayed

--- jscript.js
+++ jscript.js
@@ -30,21 +30,28 @@ function updateName()
 {
     console.log("team1 name "+team1.name)
     console.log("team2 name "+team2.name)
+    document.getElementById("team1name").textContent=team1.name;
+    document.getElementById("team2name").textContent=team2.name;
+
 }
 function updateButton()
 {
    //console.log(`${toss===1?team1.name:team2.name} BATTING`);
-
+    var button=document.getElementById("strikebutton")
+    var result = document.getElementById("result");
    if(team1.runs.length==6 && team2.runs.length==6 )
    {
-        console.log("Both team finished their chances")
+       button.remove();
+        //console.log("Both team finished their chances");
+        console.log(team1.score===team2.score?"It is a draw":`${team1.score>team2.score?team1.name:team2.name} Wins`)
+        result.textContent=team1.score===team2.score?"It is a draw":`${team1.score>team2.score?team1.name:team2.name} Wins`
    }
    else{
        toss=team1.runs.length === 6 ? 2:
        team2.runs.length === 6 ? 1 : toss;
    }
    console.log(`${toss===1?team1.name:team2.name} BATTING`);
-
+   document.getElementById("strikebutton").textContent=`${toss===1?team1.name:team2.name} BATTING `
 }
 
 handlestrike=()=>{
@@ -67,6 +74,20 @@ handlestrike=()=>{
 function updateScores(){
     console.log("team1.score " + team1.score)
     console.log("team1.score " + team2.score)
+    document.getElementById("team1score").textContent=team1.score;
+    document.getElementById("team2score").textContent=team2.score;
+    updateRuns()
+}
+function updateRuns(){
+    var team1runs =document.getElementById("team1runs").children
+    var team2runs =document.getElementById("team2runs").children
+    team1.runs.forEach((run,index)=>{
+        team1runs[index].textContent=run
+    });
+    team2.runs.forEach((run,index)=>{
+        team2runs[index].textContent=run
+    });
+
 }
 var calcScore=(runs)=>{
     return runs.map(num=>{
